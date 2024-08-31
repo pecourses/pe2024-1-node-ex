b9aa4574b45621354df959c350098a87ce652899
Viktoriia <freshitpte@gmail.com>
2024-08-31 19:00:10 +0300

    Add commonjs modules

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello world ');
+// ~ def imp
+// const sum = require('./math.js'); // module.exports
+
+// console.log(sum(1, 2));
+
+// ~ named imp
+// const Math = require('./math.js');
+
+// console.log(Math.sum(10, 20));
+// console.log(Math.mult(10, 20));
+
+// req
+const { sum, mult } = require('./math.js');
+console.log(sum(10, 20));
+console.log(mult(10, 20));
---
+++ math.js
@@ -0,0 +1,15 @@
+// ~ def exp
+
+// const sum = (a, b) => a + b;
+
+// module.exports = sum;
+
+// ~ named exp
+// const sum = (a, b) => a + b;
+// const mult = (a, b) => a * b;
+
+// module.exports = { sum, mult };
+
+// export const thunk = ...
+module.exports.sum = (a, b) => a + b;
+module.exports.mult = (a, b) => a * b;
