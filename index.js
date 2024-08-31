de9432af5d3e59dabb5b63a1da7d087b6f5898f3
Viktoriia <freshitpte@gmail.com>
2024-08-31 20:01:09 +0300

    Add node native modules

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-// ~ def imp
-// const sum = require('./math.js'); // module.exports
+const os = require('node:os');
+const fs = require('node:fs');
+const fsPromises = require('node:fs/promises');
+const path = require('node:path');
 
-// console.log(sum(1, 2));
+console.log(os.cpus());
+console.log(os.version());
 
-// ~ named imp
-// const Math = require('./math.js');
+console.log(global);
+console.log(process.env); // змінні оточення
 
-// console.log(Math.sum(10, 20));
-// console.log(Math.mult(10, 20));
+console.log(__dirname);
+console.log(__filename);
 
-// req
-const { sum, mult } = require('./math.js');
-console.log(sum(10, 20));
-console.log(mult(10, 20));
+// sync
+try {
+  const file = fs.readFileSync('./math.js', { encoding: 'utf-8' });
+  console.log(file);
+} catch (err) {
+  console.log(err);
+}
+
+// async callback
+// error first
+fs.readFile('./math.js', { encoding: 'utf-8' }, (err, file) => {
+  if (err) {
+    return console.log('err :>> ', err);
+  }
+  console.log('file :>> ', file);
+});
+
+//async promises
+fsPromises
+  .readFile('./math.js', { encoding: 'utf-8' })
+  .then(file => console.log(file))
+  .catch(err => console.log(err));
+
+// path.join() - формує шлях
+// path.resolve() - формує абсолютний шлях
