33948cc8813cc2c5838b6b33e7ff787ee8fb78f6
Viktoriia <freshitpte@gmail.com>
2024-09-01 16:20:53 +0300

    Add server with 2 pages

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
 // req
 const http = require('node:http');
+const { readFile } = require('fs/promises');
 const fs = require('node:fs');
+const path = require('node:path');
 
-const server = http.createServer((request, response) => {
-  // console.log('request.url :>> ', request.url);
-  // console.log('request.method :>> ', request.method);
+// GET  + /users => getUser
+// POST + /users => createUser
 
-  fs.readFile('./pages/index.html', { encoding: 'utf-8' }, (err, file) => {
-    if (err) {
-      response.statusCode = 500;
-      return response.end('Read file error');
+const requestListener = (requst, response) => {
+  const { method, url } = requst;
+
+  switch (method) {
+    case 'GET': {
+      const pageName = url === '/' ? 'index.html' : url;
+
+      // html
+      if (/^.*\.html$/.test(pageName)) {
+        const pagePath = path.join(__dirname, '/pages/', pageName);
+
+        if (fs.existsSync(pagePath, { encoding: 'utf-8' })) {
+          readFile(pagePath)
+            .then(data => {
+              // - файл є 200
+              response.statusCode = 200;
+              response.setHeader('Content-Type', 'text/html');
+              response.end(data);
+            })
+            .catch(err => {
+              // - файл є, але помилки при читанні 500
+              response.statusCode = 500;
+              response.end('Sever Error');
+            });
+        } else {
+          // - файла немає 404
+          response.statusCode = 404;
+          response.end('Page not found');
+        }
+      } else {
+        // не html 403
+        response.statusCode = 403;
+        response.end('You have no access');
+      }
+      break;
     }
+    default: {
+      response.statusCode = 400;
+      response.end('Bad Request');
+    }
+  }
+};
 
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/html');
-    response.end(file);
-  });
-
-  // response.statusCode = 200;
-  // response.end('Response');
-});
-
+const server = http.createServer(requestListener);
 // localhost = 127.0.0.1
 server.listen(5000, '127.0.0.1', () => {
   console.log('Server is listening http://127.0.0.1:5000');
