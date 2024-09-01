41995de1533b15360e624dccd8f302ef2acd85d8
Viktoriia <freshitpte@gmail.com>
2024-09-01 16:05:58 +0300

    Add server with file

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 // req
 const http = require('node:http');
+const fs = require('node:fs');
 
 const server = http.createServer((request, response) => {
-  console.log('request.url :>> ', request.url);
-  console.log('request.method :>> ', request.method);
+  // console.log('request.url :>> ', request.url);
+  // console.log('request.method :>> ', request.method);
 
-  response.statusCode = 200;
-  response.end('Response');
+  fs.readFile('./pages/index.html', { encoding: 'utf-8' }, (err, file) => {
+    if (err) {
+      response.statusCode = 500;
+      return response.end('Read file error');
+    }
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    response.end(file);
+  });
+
+  // response.statusCode = 200;
+  // response.end('Response');
 });
 
 // localhost = 127.0.0.1
