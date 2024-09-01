bda398270e56af53df81d58d3370cf57165c76ee
Viktoriia <freshitpte@gmail.com>
2024-09-01 15:45:04 +0300

    Add simple server

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-const os = require('node:os');
-const fs = require('node:fs');
-const fsPromises = require('node:fs/promises');
-const path = require('node:path');
+// req
+const http = require('node:http');
 
-console.log(os.cpus());
-console.log(os.version());
+const server = http.createServer((request, response) => {
+  console.log('request.url :>> ', request.url);
+  console.log('request.method :>> ', request.method);
 
-console.log(global);
-console.log(process.env); // змінні оточення
-
-console.log(__dirname);
-console.log(__filename);
-
-// sync
-try {
-  const file = fs.readFileSync('./math.js', { encoding: 'utf-8' });
-  console.log(file);
-} catch (err) {
-  console.log(err);
-}
-
-// async callback
-// error first
-fs.readFile('./math.js', { encoding: 'utf-8' }, (err, file) => {
-  if (err) {
-    return console.log('err :>> ', err);
-  }
-  console.log('file :>> ', file);
+  response.statusCode = 200;
+  response.end('Response');
 });
 
-//async promises
-fsPromises
-  .readFile('./math.js', { encoding: 'utf-8' })
-  .then(file => console.log(file))
-  .catch(err => console.log(err));
-
-// path.join() - формує шлях
-// path.resolve() - формує абсолютний шлях
+// localhost = 127.0.0.1
+server.listen(5000, '127.0.0.1', () => {
+  console.log('Server is listening http://127.0.0.1:5000');
+});
